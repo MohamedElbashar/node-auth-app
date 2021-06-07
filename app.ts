a6b9833f24fcfea5221ad78c1b536d234a320651
Mohamed Bashar <mohamedbashar200@gmail.com>
2021-06-07 19:48:16 +0200

    cookies

--- app.ts
+++ app.ts
@@ -4,12 +4,14 @@ import express from "express";
 import { Request, Response } from "express";
 import mongoose from "mongoose";
 import authRoutes from "./routes/authRoutes";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 // middleware
 app.use(express.json());
 app.use(express.static("public"));
-
+app.use(cookieParser());
 // view engine
 app.set("view engine", "ejs");
 
@@ -29,3 +31,20 @@ mongoose
 app.get("/", (req: Request, res: Response) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+//cookies
+app.get("/set-cookies", (req: Request, res: Response) => {
+  res.cookie("newUser", false);
+  res.cookie("isEmpolyee", true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    httpOnly: true,
+  });
+  // res.setHeader("Set-Cookie", "newUser=true");
+  res.send("you got the cookies!");
+});
+
+app.get("/read-cookies", (req: Request, res: Response) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
